cb91bcf72c066783d9cbbb2424338aa579739b26
pranjalsingh-techchefz <pranjal.singh@techchefz.com>
2025-09-12 18:17:16 +0530

    homepage & title

---
+++ tests/Homepage.spec.js
@@ -0,0 +1,22 @@
+const {test,expect} = require('@playwright/test')
+
+test ('Home Page',async ({page})=>{
+
+    await page.goto('https://stagecdn.royalenfield.com/in/en/reown/');
+
+    const pageTitle=page.title();
+
+    console.log('Page title is:', pageTitle);
+
+    await expect(page).toHaveTitle('Sell/Buy Motorcycle in India | Royal Enfield India');
+
+    const pageURL=page.url();
+
+    console.log('Page URL is:', pageURL);
+
+    await expect (page).toHaveURL('https://stagecdn.royalenfield.com/in/en/reown/');
+
+    await page.close();
+
+}
+)
